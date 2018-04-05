4c6f73af21f3d76bf265e6a4bc891817c9b6bc68
Jose Suarez <jose95sp@outlook.com>
2018-04-05 03:37:39 -0500

    project finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employe } from './shared/models/employe';
 
 @Component({
   selector: 'sp-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sp';
+  employeArray: Employe[] = [
+    {id: 1, name: 'Jose', country: 'Peru'},
+    {id: 2, name: 'Victor', country: 'Bolivia'},
+    {id: 3, name: 'Angy', country: 'Chile'},
+  ];
+  selectedEmploye: Employe = new Employe();
+  public addOrEdit() {
+    if (this.selectedEmploye.id == null) {
+      this.selectedEmploye.id = this.employeArray.length + 1;
+      this.employeArray.push(this.selectedEmploye);
+    }
+    this.selectedEmploye = new Employe();
+  }
+  public openToEdit(employe: Employe) {
+    this.selectedEmploye = employe;
+  }
+  public delete(employe: Employe) {
+    this.employeArray = this.employeArray.filter(x => x !== this.selectedEmploye);
+    this.selectedEmploye = new Employe();
+  }
 }
